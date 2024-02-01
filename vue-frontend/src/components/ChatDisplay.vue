ed8f0678ae78d95711f561be27d33ae1f88a57af
reyhoondavaee@gmail.com <reyhoondavaee@gmail.com>
2024-02-01 23:52:31 +0330

    chats are shown

--- vue-frontend/src/components/ChatDisplay.vue
+++ vue-frontend/src/components/ChatDisplay.vue
@@ -2,7 +2,9 @@
     <div class="chat-display">
       <div v-if="selectedChat">
         <h2>{{ selectedChat.name }}</h2>
-        <!-- Display chat content or any other details -->
+        <div v-for="message in selectedChat.messages" :key="message.id">
+          <p>{{ message.sender.username }}: {{ message.content }}</p>
+        </div>
       </div>
       <div v-else>
         <p>Select a chat to start the conversation</p>
@@ -21,14 +23,15 @@
   <style scoped>
   .chat-display {
     padding: 20px;
-    right:0;
-    position:absolute;
+    right: 0;
+    position: absolute;
     bottom: 0;
     height: 85%;
-    margin-top:10px;
+    margin-top: 10px;
     position: absolute;
     grid-column-start: 4;
-    width:76%;
+    width: 76%;
   }
+  
   </style>
   
